a1ec7653fb03fedbf3086e8147c280bf4b953a90
Sunil Rana <sunil.rana320778@gmail.com>
2025-07-22 16:47:08 +0530

    feat: Add data classes for upcoming movie responses

---
+++ app/src/main/java/com/example/philm/data/network/dto/movie/UpcomingMovieResponse.kt
@@ -0,0 +1,21 @@
+package com.example.philm.data.network.dto.movie
+
+import com.google.gson.annotations.SerializedName
+
+data class UpcomingMovieResponse(
+
+    @SerializedName("dates")
+    val dates: Dates? = null,
+
+    @SerializedName("page")
+    val page: Int? = null,
+
+    @SerializedName("total_pages")
+    val totalPages: Int? = null,
+
+    @SerializedName("results")
+    val results: List<Result>? = null,
+
+    @SerializedName("total_results")
+    val totalResults: Int? = null
+)
