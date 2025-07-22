c09dd80dac1ad82e94b639b147dec3fccbbea8f3
Sunil Rana <sunil.rana320778@gmail.com>
2025-07-22 16:47:19 +0530

    feat: Add data classes for top rated movie responses

---
+++ app/src/main/java/com/example/philm/data/network/dto/movie/TopRatedMovieResponse.kt
@@ -0,0 +1,17 @@
+package com.example.philm.data.network.dto.movie
+
+import com.google.gson.annotations.SerializedName
+
+data class TopRatedMovieResponse(
+    @SerializedName("page")
+    val page: Int? = null,
+
+    @SerializedName("total_pages")
+    val totalPages: Int? = null,
+
+    @SerializedName("results")
+    val results: List<Result>? = null,
+
+    @SerializedName("total_results")
+    val totalResults: Int? = null
+)
